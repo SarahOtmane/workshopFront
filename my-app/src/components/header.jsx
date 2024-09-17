c266b13fd6d8f33e1ec05996ef5aee30e7a9ee9f
SarahOtmane <sarahotmane02@gmail.com>
2024-09-17 23:55:05 +0200

    "Added Header component with responsive navigation menu and corresponding CSS styles"

---
+++ my-app/src/components/header.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import '../css/frontoffice/header.css';
+import logo from '../assets/logo.webp';
+import logo_short from '../assets/logo_short.webp';
+
+export default function Header(){
+    const [openPopup, setOpenPopup] = useState(false);
+
+    return(
+        <header className="row">
+            <img src={openPopup ? logo : logo_short} alt='logo' />
+            <nav className='navigationMobile'>
+                <div id="menuToggle">
+                    <input type="checkbox" onClick={() => setOpenPopup(!openPopup)} />
+                        <span></span>
+                        <span></span>
+                        <span></span>
+
+                        <ul id='menu' className={`${openPopup ? 'open' : ''}`}>
+                            <li><a href="/personnalisation">Le concept</a></li>
+                        </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
