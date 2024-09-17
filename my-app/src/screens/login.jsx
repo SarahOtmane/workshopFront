783eb739d799e1e3f9f6209eaceadb891274b56c
SarahOtmane <sarahotmane02@gmail.com>
2024-09-17 16:26:53 +0200

    login component

---
+++ my-app/src/screens/login.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+export default function Login(){
+    return(
+        <main className="login w-100 h-100">
+            <form className="m-auto">
+
+            </form>
+        </main>
+    )
+}
