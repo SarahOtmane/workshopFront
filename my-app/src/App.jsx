984c60201e7c6dad8d979caf09950de312b2fa2d
SarahOtmane <sarahotmane02@gmail.com>
2024-09-18 00:05:57 +0200

    add Home component to the routes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,12 +4,14 @@ import './App.css';
 
 import Login from "./screens/backoffice/login";
 import Header from "./components/header";
+import Home from "./screens/frontoffice/home";
 
 export default function App(){
     return(
         <BrowserRouter>
             <Header />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/admin" element={<Login />} />
             </Routes>
         </BrowserRouter>
