b84d3412ae59e9f5bc4afdd557bbb8a805b43011
SarahOtmane <sarahotmane02@gmail.com>
2024-09-17 23:17:51 +0200

    import header component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 
 import Login from "./screens/backoffice/login";
+import Header from "./components/header";
 
 export default function App(){
     return(
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/admin" element={<Login />} />
             </Routes>
