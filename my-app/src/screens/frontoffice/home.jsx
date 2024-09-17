71c898dca837aa853b2051b63a67fcf8fb80611c
SarahOtmane <sarahotmane02@gmail.com>
2024-09-18 00:10:38 +0200

    "Added styles for .home h1 element in home.css"

--- my-app/src/screens/frontoffice/home.jsx
+++ my-app/src/screens/frontoffice/home.jsx
@@ -1,11 +1,11 @@
 
 
-
+import '../../css/frontoffice/home.css';
 
 export default function Home(){
     return(
-        <main>
-            
+        <main className="home">
+            <h1 className="text_uppercase m-auto">Construit ta propre console</h1>
         </main>
     )
 }
