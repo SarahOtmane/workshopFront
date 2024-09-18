6939617c0a8ad9bc2ea92e105acaf392e420eea0
Mattéo Houillon <maatteoo.h@gmail.com>
2024-09-18 14:32:08 +0200

    Add card product

---
+++ my-app/src/components/backoffice/inputProduct.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default function InputProduct({label, type = "text", placeholder = "", onChange, name, value}){
+    return(
+        <div className="inputLabel column">
+            <label><h2>{label}</h2></label>        
+                <input 
+                    type={type} 
+                    placeholder={placeholder} 
+                    name={name} 
+                    onChange={onChange}
+                    value={value}
+                    className={type === 'color' ? 'color-input' : 'inputField'}
+                />
+        </div>
+    )
+}
+
+InputProduct.propTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
--- my-app/src/screens/backoffice/addAccessory.jsx
+++ my-app/src/screens/backoffice/addAccessory.jsx
@@ -1,14 +1,64 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import '../../css/backoffice/screen.css';
-import AddCard from '../../components/backoffice/addCard';
+import InputProduct from '../../components/backoffice/inputProduct';
 
 export default function AddAccessory(){
 
+    const [accessory, setAccessory] = useState({
+        name: "",
+        color: "#000000",
+        price: 0,
+        image: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAccessory(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+        console.log(accessory);
+    };
+
     return(
         <main className="w-100 h-100">
-
+            <div className='centered'>
+                <h1>Add Accessory</h1>
+            </div>
+            <div className='card'>
+            <form>
+                <InputProduct 
+                    label='Nom' 
+                    placeholder='nom de votre produit' 
+                    onChange={handleChange}
+                    name='name'
+                    value={accessory.name}
+                />
+                <InputProduct 
+                    label='Prix'
+                    placeholder='prix de votre produit' 
+                    type='number' 
+                    onChange={handleChange}
+                    name='price'
+                    value={accessory.price}
+                />
+                <InputProduct 
+                    label='Image'
+                    type='file'
+                    onChange={handleChange}
+                    name='image'
+                    value={accessory.image}
+                />
+                <InputProduct
+                    label='Couleur'
+                    type='color' 
+                    onChange={handleChange} 
+                    name='color'
+                    value={accessory.color}
+                />
+            </form>
+            </div>
         </main>
     )
 }
