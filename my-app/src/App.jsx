8c8834eef4787beaeb805fe6198588440f9d944b
Mattéo Houillon <maatteoo.h@gmail.com>
2024-09-18 12:11:34 +0200

    add new add accessory screen

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./screens/backoffice/login";
 import Header from "./components/header";
 import Home from "./screens/frontoffice/home";
 import Panel from "./screens/backoffice/panel";
+import AddAccessory from "./screens/backoffice/addAccessory";
 
 export default function App(){
     return(
@@ -15,6 +16,7 @@ export default function App(){
                 <Route path="/" element={<Home />} />
                 <Route path="/admin" element={<Login />} />
                 <Route path="/admin/panel" element={<Panel />} />
+                <Route path="/admin/add-accessory" element={<AddAccessory />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ my-app/src/screens/backoffice/addAccessory.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import '../../css/backoffice/screen.css';
+import AddCard from '../../components/backoffice/addCard';
+
+export default function AddAccessory(){
+
+    return(
+        <main className="w-100 h-100">
+
+        </main>
+    )
+}
