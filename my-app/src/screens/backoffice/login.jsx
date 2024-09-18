7fd1c97647a3e2e6f665161b119b55a21d126b33
Mattéo Houillon <maatteoo.h@gmail.com>
2024-09-18 10:04:26 +0200

    global class to center element in a div add

--- my-app/src/screens/backoffice/login.jsx
+++ my-app/src/screens/backoffice/login.jsx
@@ -43,8 +43,7 @@ export default function Login(){
     return(
         <main className="login w-100 h-100">
             <form className="m-auto">
-                <div className="head row">
-                    <img src={logo} alt='logo' />
+                <div className="head row centered">
                     <h1>Admin</h1>
                 </div>
 
