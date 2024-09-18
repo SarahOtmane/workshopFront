3a5bcd03f9589d547c324019c83eb003eb4543be
Mattéo Houillon <maatteoo.h@gmail.com>
2024-09-18 11:52:17 +0200

    add card component in panel page

--- my-app/src/screens/backoffice/panel.jsx
+++ my-app/src/screens/backoffice/panel.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import '../../css/backoffice/screen.css';
-import AddAccessory from '../../components/backoffice/addAccessory';
+import AddCard from '../../components/backoffice/addCard';
 
 
 export default function Panel(){
@@ -9,7 +9,11 @@ export default function Panel(){
 
     return(
         <main className="w-100 h-100">
-            <AddAccessory />
+            <div className="head row centered">
+                <h1 className='text_uppercase'>panel</h1>
+            </div>
+            <AddCard type="accessory" title="ajouter un accesoire" />
+            <AddCard type="console" title="ajouter une console" />
         </main>
     )
 }
