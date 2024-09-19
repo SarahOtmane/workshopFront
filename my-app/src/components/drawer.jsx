fb6d260b32fb9ce60d11d7a2984e845c3aee9cbc
Mattéo Houillon <maatteoo.h@gmail.com>
2024-09-19 11:46:12 +0200

    add drawer style

---
+++ my-app/src/components/drawer.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import '../css/frontoffice/header.css';
+
+export default function Drawer(){
+    const [openPopup, setOpenPopup] = useState(false);
+    const path = useLocation().pathname;
+    
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return(
+        <nav className='navigationMobile'>
+        <div id="menuToggle">
+            <input type="checkbox" onClick={() => setOpenPopup(!openPopup)} />
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul id='menu' className={`${openPopup ? 'open' : ''}`}>
+                <li><a href="/" className={path === '/' ? 'active' : ''}>Acceuil</a></li>
+                <li><a href="/ok" className={path === '/personalisation' ? 'active' : ''}>Personnalisation</a></li>
+            </ul>
+            <div className="overlay"></div>
+        </div>
+    </nav>
+    )
+}
--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import '../css/frontoffice/header.css';
 import logo from '../assets/logo.webp';
 import logo_short from '../assets/logo_short.webp';
+import Drawer from './drawer';
 
 export default function Header(){
     const [openPopup, setOpenPopup] = useState(false);
@@ -23,19 +24,7 @@ export default function Header(){
         <header>
             <div className="header row">
                 <img src={isMobile ? logo_short : logo} alt='logo' />
-                <nav className='navigationMobile'>
-                    <div id="menuToggle">
-                        <input type="checkbox" onClick={() => setOpenPopup(!openPopup)} />
-                        <span></span>
-                        <span></span>
-                        <span></span>
-
-                        <ul id='menu' className={`${openPopup ? 'open' : ''}`}>
-                            <li><a href="/">Personnalisation</a></li>
-                        </ul>
-                        <div className="overlay"></div>
-                    </div>
-                </nav>
+                <Drawer />
             </div>
         </header>
     )
