f55ab0e542e5cc5bf2e554e3e68a82d922f95a21
Mattéo Houillon <maatteoo.h@gmail.com>
2024-09-19 10:28:01 +0200

    revert header changes

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import '../css/frontoffice/header.css';
 import logo from '../assets/logo.webp';
@@ -6,26 +6,13 @@ import logo_short from '../assets/logo_short.webp';
 
 export default function Header(){
     const [openPopup, setOpenPopup] = useState(false);
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
 
     return(
-        <header>
-            <div className="header row">
-                <img src={isMobile ? (openPopup ? logo : logo_short) : logo} alt='logo' />
-                <nav className='navigationMobile'>
-                    <div id="menuToggle">
-                        <input type="checkbox" onClick={() => setOpenPopup(!openPopup)} />
+        <header className="header row">
+            <img src={openPopup ? logo : logo_short} alt='logo' />
+            <nav className='navigationMobile'>
+                <div id="menuToggle">
+                    <input type="checkbox" onClick={() => setOpenPopup(!openPopup)} />
                         <span></span>
                         <span></span>
                         <span></span>
@@ -33,10 +20,8 @@ export default function Header(){
                         <ul id='menu' className={`${openPopup ? 'open' : ''}`}>
                             <li><a href="/">Personnalisation</a></li>
                         </ul>
-                        <div className="overlay"></div>
-                    </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </header>
     )
 }
