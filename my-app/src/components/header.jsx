fb2c89ce2990ccaf7db8ba4f397eba4e3f1d796a
SarahOtmane <sarahotmane02@gmail.com>
2024-09-20 13:34:01 +0200

    update

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import { Link } from 'react-router-dom'; 
-import '../css/frontoffice/header.css';
-import logo from '../assets/logo.webp';
-import logo_short from '../assets/logo.webp';
-=======
-import { useState, useEffect } from "react";
 
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'; 
 import '../css/frontoffice/header.css';
 import logo from '../assets/logo.webp';
 import logo_short from '../assets/logo_short.webp';
 import Drawer from './drawer';
->>>>>>> upstream/header
 
 export default function Header() {
     const [openPopup, setOpenPopup] = useState(false);
@@ -28,36 +21,12 @@ export default function Header() {
         };
     }, []);
 
-<<<<<<< HEAD
-    return (
-        <header className="header row">
-            <Link to='/'>
-                <img src={openPopup ? logo : logo_short} alt='logo' />
-            </Link>
-            <nav className='navigationMobile'>
-                <div id="menuToggle">
-                    <input type="checkbox" onClick={() => setOpenPopup(!openPopup)} />
-                        <span></span>
-                        <span></span>
-                        <span></span>
-
-                        <ul id='menu' className={`${openPopup ? 'open' : ''}`}>
-                            <li><Link to="/">Personnalisation</Link></li>
-                            <li><Link to="/">Personnalisation</Link></li>
-                            <li><Link to="/">Personnalisation</Link></li>
-                            <li><Link to="/">Personnalisation</Link></li>
-
-                        </ul>
-                </div>
-            </nav>
-=======
     return(
         <header>
             <div className="header row">
                 <img src={isMobile ? logo_short : logo} alt='logo' />
                 <Drawer />
             </div>
->>>>>>> upstream/header
         </header>
     )
 }
